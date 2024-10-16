b6ee912bf95b235cc18349dee3f59f09c4fc5220
unknown <amirarshia3155@gmail.com>
2024-10-15 23:30:23 -0700

    first real commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "static/index.html")
+    })
+
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    // Add handler for CSS file
+    http.HandleFunc("/static/style.css", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "static/styles.css")
+    })
+
+    fmt.Println("Server is running on http://localhost:8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
+}
